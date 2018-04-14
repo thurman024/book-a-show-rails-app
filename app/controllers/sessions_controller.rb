f0e86e5cb1f9b0072631a94392d30f4a07e603a2
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-04-13 21:50:40 -0500

    sessions controller built

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.find_by(name: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+     session[:user_id] = @user.id
+     redirect_to bookings_path
+    else
+     redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.delete :user_id if session[:user_id]
+    redirect_to root_path
+  end
 end
