819036d6c2f28847ef6bfc5c21a86cb11f4307e5
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-04-13 22:45:43 -0500

    venues controller built

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,3 +1,27 @@
 class VenuesController < ApplicationController
+  def index
+    @venues = Venue.all
+  end
 
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to venue_path(@venue)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  private
+  def venue_params
+    params.require(:venue).permit(:name, :description)
+  end
 end
