7cbde61d7b393397c7f552f4ccd79d4fb09fc9a1
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-04-14 15:45:32 -0500

    bookings index action and view added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,17 @@
 class BookingsController < ApplicationController
+  def index
+    if params[:venue_id]
+      @venue = Venue.find_by(id: params[:venue_id])
+      redirect_to venues_path, alert: "Venue not found" if @venue.nil?
+      @bookings = @venue.bookings
+    elsif params[:band_id]
+      @band = Band.find_by(id: params[:band_id])
+      redirect_to bands_path, alert: "Band not found" if @band.nil?
+      @bookings = @band.shows
+    else
+      @bookings = Booking.all
+    end
+  end
+
 
 end
