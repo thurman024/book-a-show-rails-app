587cf27d9591f9744d5b1b2f760105d99990ebf9
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-04-13 21:05:07 -0500

    controller files and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bands do
+    resources :bookings
+  end
+
+  resources :venues do
+    resources :bookings
+  end
+
+  resources :bookings
+
+  resources :users, only: [:new, :create]
+
+  get "login" => "sessions#new", as: "login"
+  post "login" => "sessions#create"
+  get "logout" => "sessions#destroy", as: "logout"
+
+  root "static#welcome"
 end
