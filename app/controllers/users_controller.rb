394b28e88975924b824936673d831aa775917a9e
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-04-13 21:15:07 -0500

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :venue_owner)
+  end
 end
