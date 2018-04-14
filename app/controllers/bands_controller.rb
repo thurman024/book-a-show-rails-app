815fa17f26a12db421184f10bece7adbfea4d9a2
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-04-13 22:00:28 -0500

    bands controller added

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,3 +1,22 @@
 class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+  end
 
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    if @band.save
+      redirect_to band_path(@band)
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
 end
