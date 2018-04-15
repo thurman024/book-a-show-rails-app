ab1b8a8eae3aa9b76f3bcc11e9443b9a9ee9c9ce
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-04-15 13:26:44 -0500

    model validations added

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,4 +1,6 @@
 class Band < ApplicationRecord
   has_many :shows, class_name: :Booking
 
+  validates :name, presence: true
+  validates :rate, numericality: { only_integer: true}
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,6 @@
 class Venue < ApplicationRecord
   has_many :bookings
   has_many :bands, through: :bookings
+
+  validates :name, presence: true
 end
