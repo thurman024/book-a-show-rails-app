a0b1ccbc07448efaa476ab25aebe213300ecfce3
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-04-16 09:10:57 -0500

    show count method added

--- app/models/band.rb
+++ app/models/band.rb
@@ -4,9 +4,22 @@ class Band < ApplicationRecord
   validates :name, presence: true
   validates :rate, numericality: { only_integer: true}
 
-  # def next_show
-  #   current_time = Time.now
-  #   shows = self.shows.order("showtime")
-  #   shows.where("showtime > ?", current_time).limit(1)
-  # end
+  def show_count
+    @count = self.shows.count
+    if @count == 0
+      "No scheduled shows"
+    else
+      pluralize(@count, 'show') + "scheduled"
+    end
+  end
+
+  def next_show
+    if @count == 0
+      "There are no scheduled shows for this band"
+    else
+      current_time = Time.now
+      shows = self.shows.order("showtime")
+      shows.where("showtime > ?", current_time).limit(1)
+    end
+  end
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,9 +4,21 @@ class Venue < ApplicationRecord
 
   validates :name, presence: true
 
-  # def next_show
-  #   current_time = Time.now
-  #   shows = self.bookings.order("showtime")
-  #   shows.where("showtime > ?", current_time).limit(1)
-  # end
+  def show_count
+    @count = self.shows.count
+    if @count == 0
+      "No scheduled shows"
+    else
+      pluralize(@count, 'show') + "scheduled"
+    end
+  end
+
+   def next_show
+     if @count == 0
+       "There are no scheduled shows for this band"
+     else
+       current_time = Time.now
+       shows = self.bookings.order("showtime")
+       shows.where("showtime > ?", current_time).limit(1)
+   end
 end
