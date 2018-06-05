7d00cb1516d7e70ad56143022748d32ff974c08c
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-06-04 21:38:47 -0500

    add js to render list of band's shows

---
+++ app/assets/javascripts/bookings.js
@@ -0,0 +1,29 @@
+$(function() {
+  $(".js-all").on('click', function(event) {
+    //alert("you clicked the link")
+    event.preventDefault()
+    //console.log(this.href)
+    $.get(this.href, shows => {
+
+      shows.forEach( show => {
+        let newShow = new Show(show)
+        let showHtml = newShow.formatShows()
+        $("div.shows-list").append(showHtml)
+      })
+    })
+  })
+})
+
+function Show(attributes) {
+  this.id = attributes.id
+  //this.bandName = attributes.band.name
+  //this.venueName = attributes.venue.name
+  this.showtime = attributes.showtime
+}
+
+
+Show.prototype.formatShows = function () {
+  let showHtml = `<p>${this.showtime}</p>
+    `
+  return showHtml
+};
